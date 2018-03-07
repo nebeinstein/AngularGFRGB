678ce3c1ab59c22cd9951d1154c16acc24371fbe
nebeinstein <nebeinstein@wpi.edu>
2018-03-07 09:34:03 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ang-ui';
+  title = 'Gluten Free Radioactive Green Beans';
 }
