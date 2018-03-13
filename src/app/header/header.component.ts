38268b86817bb674b532e28141eb5487180b95e6
nebeinstein <nebeinstein@wpi.edu>
2018-03-13 12:35:17 -0400

    made basic header based off recipebook project

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,11 +5,27 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class HeaderComponent {
+  constructor() {
   }
 
+  // onSaveData() {
+  //     this.dataStorageService.storageRecipes().subscribe(
+  //         (response: Response) => {
+  //             console.log(response);
+  //         }
+  //     );
+  // }
+
+  // onFetchData() {
+  //     this.dataStorageService.getRecipes();
+  // }
+
+  // onLogout() {
+  //     this.authService.logout();
+  // }
+
+  // isAuthenticated() {
+  //     return this.authService.isAuthenticated();
+  // }
 }
